c1470b2db62cb5e4b0fc7f58b352fa7e92c3a45c
Greg King <gregorywilliamking@gmail.com>
2024-09-02 11:46:07 -0700

    Updated typos

--- server/models/trail.model.js
+++ server/models/trail.model.js
@@ -3,28 +3,28 @@ const TrailSchema = new Schema(
   {
     trailName: {
       type: String,
-      required: [true, 'Trail name is required!'],
-      minlength: [2, 'Trail name must be at least 2 characters long!'],
-      maxlength: [50, 'Trail name must be less than 50 characters long'],
+      required: [true, 'Trail name is required.'],
+      minlength: [2, 'Trail name must be at least 2 characters long.'],
+      maxlength: [50, 'Trail name must be less than 50 characters long.'],
     },
     length: {
       type: Number,
-      required: [true, 'Length required (mil/Km)!'],
+      required: [true, 'Length required (mil/Km).'],
     },
     location: {
       type: String,
-      required: [true, 'Location is required!'],
+      required: [true, 'Location is required.'],
     },
     difficulty: {
       type: String,
     },
     description: {
       type: String,
-      required: [true, 'Description is required!'],
+      required: [true, 'Description is required.'],
     },
     elevation: {
       type: Number,
-      required: [true, 'Elevation is required!'],
+      required: [true, 'Elevation is required.'],
     },
     image: {
       type: String,
